4b265ad714216417383ee22de2e7a9481b02b1d3
SharonWang <848696836@qq.com>
2024-03-11 08:31:12 +0000

    add allow_dangerous_setting in streamlit.py

--- streamlit_webApp.py
+++ streamlit_webApp.py
@@ -6,20 +6,24 @@ from langchain_openai import AzureOpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import AzureChatOpenAI
+from langchain.embeddings import AzureOpenAIEmbeddings
+from langchain.document_loaders import PyPDFLoader
+
 
 @st.cache_resource
-def load_data(vector_store_dir: str="data/IRM-help-pdf"):
-    db = FAISS.load_local(vector_store_dir, AzureOpenAIEmbeddings())
+def load_data(vector_store_dir: str="data/IRM-faiss"):
+    db = FAISS.load_local(vector_store_dir, AzureOpenAIEmbeddings(), allow_dangerous_deserialization= True)
     llm = AzureChatOpenAI(model_name="gpt-35-turbo", temperature=0.5)
 
     print("Loading data...")
-     
+
     AMAZON_REVIEW_BOT = RetrievalQA.from_chain_type(llm,
-                  retriever=db.as_retriever(search_type="similarity_score_threshold",
-                    search_kwargs={"score_threshold": 0.7}))
+                                                    retriever=db.as_retriever(search_type="similarity_score_threshold",
+                                                                              search_kwargs={"score_threshold": 0.7}))
     AMAZON_REVIEW_BOT.return_source_documents = True
 
     return AMAZON_REVIEW_BOT
+
 
 def chat(message, history):
     print(f"[message]{message}")
@@ -34,37 +38,37 @@ def chat(message, history):
     else:
         return "I don't know."
 
+
 if __name__ == "__main__":
     os.environ["OPENAI_API_TYPE"] = "azure"
     os.environ["OPENAI_API_VERSION"] = "2023-05-15"
     os.environ["OPENAI_API_BASE"] = "https://pvg-azure-openai-uk-south.openai.azure.com/openai"
     env_path = os.getenv("HOME") + "/Documents/src/openai/.env"
-    load_dotenv(dotenv_path=env_path, verbose=True) 
 
     st.title('IRM Help Doc search and Q&A')
 
-    prompt = st.chat_input("Enter your query here")
+    prompt = st.chat_input("Enter your questions here")
 
     if "user_prompt_history" not in st.session_state:
-       st.session_state["user_prompt_history"]=[]
+        st.session_state["user_prompt_history"] = []
     if "chat_answers_history" not in st.session_state:
-       st.session_state["chat_answers_history"]=[]
+        st.session_state["chat_answers_history"] = []
     if "chat_history" not in st.session_state:
-       st.session_state["chat_history"]=[]
+        st.session_state["chat_history"] = []
 
     if prompt:
-       with st.spinner("Generating......"):
-           output = chat(prompt, st.session_state["chat_history"])
+        with st.spinner("Generating......"):
+            output = chat(prompt, st.session_state["chat_history"])
 
-           st.session_state["chat_answers_history"].append(output)
-           st.session_state["user_prompt_history"].append(prompt)
-           st.session_state["chat_history"].append((prompt,output))
+            st.session_state["chat_answers_history"].append(output)
+            st.session_state["user_prompt_history"].append(prompt)
+            st.session_state["chat_history"].append((prompt, output))
 
     # Displaying the chat history
 
     if st.session_state["chat_answers_history"]:
-       for i, j in zip(st.session_state["chat_answers_history"],st.session_state["user_prompt_history"]):
-          message1 = st.chat_message("user")
-          message1.write(j)
-          message2 = st.chat_message("assistant")
-          message2.write(i)
+        for i, j in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
+            message1 = st.chat_message("user")
+            message1.write(j)
+            message2 = st.chat_message("assistant")
+            message2.write(i)
